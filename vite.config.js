5f432a7ff178e01b5abc77295b4900045a620531
gloumy68 <161340198+gloumy68@users.noreply.github.com>
2025-06-02 16:03:44 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { resolve } from "path";
 
 export default defineConfig({
   plugins: [react()],
+  root: ".", // Racine du projet
+  build: {
+    rollupOptions: {
+      input: resolve(__dirname, "public/index.html"),
+    },
+    outDir: "dist"
+  },
 });
