839fe96e01ebd8df4448d2028d89b69d2fe10662
gloumy68 <161340198+gloumy68@users.noreply.github.com>
2025-06-02 16:35:52 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -9,10 +9,4 @@ export default defineConfig({
       "@": path.resolve(__dirname, "src"),
     },
   },
-  build: {
-    rollupOptions: {
-      input: "public/index.html",
-    },
-    outDir: "dist",
-  },
 });
