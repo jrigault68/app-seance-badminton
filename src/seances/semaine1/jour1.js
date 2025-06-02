4b1c0280ad687518681264bb2001d03244073a64
gloumy68 <161340198+gloumy68@users.noreply.github.com>
2025-06-02 17:12:15 +0200

    Create jour1.js

---
+++ src/seances/semaine1/jour1.js
@@ -0,0 +1,16 @@
+export default [
+  {
+    name: "Étirement bras croisé",
+    duration: 30,
+    description: "Attraper le bras droit avec la main gauche et le tirer doucement vers la gauche.",
+    position: "Debout ou assis",
+    erreurs: "Ne pas lever l’épaule, ne pas tourner le buste."
+  },
+  {
+    name: "Automassage quadriceps",
+    duration: 30,
+    description: "Faire rouler le rouleau du haut vers le bas des cuisses.",
+    position: "Allongé sur le ventre, appui sur les avant-bras",
+    erreurs: "Ne pas aller trop vite, éviter les articulations."
+  }
+];
