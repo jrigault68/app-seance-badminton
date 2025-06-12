451db51ee362239496b1b9d1b8ece4088a91f25b
gloumy68 <161340198+gloumy68@users.noreply.github.com>
2025-06-12 17:12:05 +0200

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -7,27 +7,7 @@ require("./middleware/google-auth");
 
 const app = express();
 
-app.use(
-  cors({
-    origin: function (origin, callback) {
-      console.log("origin : ", origin);
-      const allowed = [
-        "http://localhost:5173",
-        "https://app-seance-badminton-3u1t.vercel.app"
-      ];
-      const isVercelPreview = /^https:\/\/.*\.vercel\.app$/.test(origin);
-      if (!origin || allowed.includes(origin) || isVercelPreview) {
-        return callback(null, true);
-      }
-      return callback(new Error("Not allowed by CORS"));
-    },
-    credentials: true,
-  })
-);
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Credentials", "true");
-  next();
-});
+app.use(cors());
 app.use(cookieParser());
 app.use(express.json());
 
