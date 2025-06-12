264ab687f265a366c4f2652c0adbfef4885d1bbc
gloumy68 <161340198+gloumy68@users.noreply.github.com>
2025-06-12 15:42:10 +0200

    2

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,7 @@ const app = express();
 
 const allowedOrigins = [
   "http://localhost:5173",
-  "https://app-seance-badminton.vercel.app",
+  "https://app-seance-badminton-3u1t.vercel.app/",
   "https://app-seance-badminton-3u1t-*-gloumy68s-projects.vercel.app/"
 ];
 
